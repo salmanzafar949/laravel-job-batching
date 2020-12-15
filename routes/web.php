0212fd163a6bd0311fa578e3215aa8b33e105c23
Salman Zafar <salman.zafar@innowi.com>
2020-12-15 16:27:57 +0500

    removing routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\SaleController;
 
 Route::get('/', fn() => view('welcome'));
-
-Route::get('upload', [SaleController::class, 'index']);
-
-Route::post('upload', [SaleController::class, 'store'])->name('upload.post');
